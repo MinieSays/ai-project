e14a76b7d0970da7635dc4013f79ff3e709f8117
Minie <sayadeth@users.noreply.github.com>
2024-09-20 11:22:46 -0400

    edited mongoose files and env

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,14 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/', '/api/webhooks/clerk', '/api/webhooks/stripe'])
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect()
+  }
+})
 
 export const config = {
   matcher: [
